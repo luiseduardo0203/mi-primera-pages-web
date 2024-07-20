c40fc1467c630df20b79102f152a00e6ec37375d
luiseduardo0203 <luiseduardo02032007@gmail.com>
2024-07-20 11:56:21 -0400

    hice una barra de busqueda

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name= "home.html"
+
+class AboutPageView(TemplateView):
+    template_name= "about.html"
